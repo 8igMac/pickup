eb80608a58af26bdc84b817a1a1a2a67cd80921c
8igmac <8igmac@pm.me>
2022-03-27 19:35:57 +0800

    [FEAT] Homepage UI.

--- lib/driver_home_page.dart
+++ lib/driver_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pickup/color.dart';
 
 class DriverHomePage extends StatelessWidget {
   const DriverHomePage({
@@ -7,12 +9,103 @@ class DriverHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      foregroundDecoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/driver_home_page.png'),
-          fit: BoxFit.fill,
-        ),
+    // return Container(
+    //   foregroundDecoration: BoxDecoration(
+    //     image: DecorationImage(
+    //       image: AssetImage('assets/images/driver_home_page.png'),
+    //       fit: BoxFit.fill,
+    //     ),
+    //   ),
+    // );
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 32.w,
+        right: 32.w,
+        top: 46.h,
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: 364.w,
+            height: 509.h,
+            decoration: BoxDecoration(
+              color: MyColors.yellow,
+              borderRadius: BorderRadius.circular(30.r),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 24.h),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    fixedSize:
+                        MaterialStateProperty.all<Size>(Size(325.w, 56.h)),
+                  ),
+                  onPressed: null,
+                  child: Text(
+                    'EDIT SCHEDULE',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 24.h),
+                Container(
+                  width: 325.w,
+                  height: 200.h,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.r),
+                  ),
+                  foregroundDecoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/calender.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 42.h),
+                Container(
+                  width: 270.w,
+                  height: 80.h,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.r),
+                  ),
+                  foregroundDecoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/mile.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 12.h),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    fixedSize:
+                        MaterialStateProperty.all<Size>(Size(295.w, 44.h)),
+                  ),
+                  onPressed: null,
+                  child: Text(
+                    'ADD EVENT',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 38.h),
+          Container(
+            width: 364.w,
+            height: 150.h,
+            foregroundDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.r),
+              image: DecorationImage(
+                image: AssetImage('assets/images/map.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
