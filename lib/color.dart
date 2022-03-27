e24b249f1e43be736ab2d465e3a45a1100b3423f
8igmac <8igmac@pm.me>
2022-03-27 19:35:57 +0800

    [FEAT] Homepage UI.

---
+++ lib/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  // This class is not meant to be instantiated or extended; this constructor
+  // prevents instantiation and extension.
+  MyColors._();
+
+  static const Color yellow = Color(0xFFFEE45C);
+}
